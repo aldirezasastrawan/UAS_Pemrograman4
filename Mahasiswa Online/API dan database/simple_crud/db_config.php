768e0c11757f2b2224332666712a0bba77b672f6
mrahmatwu <m.rahmatwu@gmail.com>
2016-06-21 01:20:10 +0700

    Tugas UAS P4

---
+++ Mahasiswa Online/API dan database/simple_crud/db_config.php
@@ -0,0 +1,11 @@
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('root', "admincHKnULq"); // db user
+define('', "jPFqNIiSealI"); // db password
+define('database_mahasiswa', "mahasiswa"); // database name
+define('localhost', "127.9.203.130:3306"); // db server
+?>
---
+++ Mock Up Mahasiswa Online/README.md
@@ -0,0 +1,1 @@
+# uas-pemrograman4-android
